2f4fffaea32ecaf016595bbd979126b050b6a66a
Eridanus_Sora <h@moe.re>
2015-07-28 11:24:51 +0800

    修正调用逻辑

--- Main.py
+++ Main.py
@@ -1,10 +1,39 @@
-import config,meta
+import config,meta,database
 from log import Log 
-import sys
+import sys,time,socket,threading
+
+def tcpLinkHandler(socket, address, Logger):
+    Logger.info('接受来自 %s:%s 的连接' % address)
+    while True:
+        data = socket.recv(1024)
+        if data:
+            try:
+                api_id = data.decode('utf-8')
+                Logger.info('刷新API:' + api_id)
+                renewData(api_id, Logger)
+            except Exception as e:
+                Logger.error('刷新API数据失败' + str(e))
+        else:
+            break
+    socket.close()
+
+def renewData(api_id, Logger):
+    try:
+        spiderName = database.getSpiderName(api_id)
+        Logger.info('获得映射 : [' + api_id + '->' + spiderName + '] 尝试刷新')
+        Spider = __import__(spiderName + 'Spider')
+        Spider = getattr(Spider, spiderName + 'Spider')()
+        Spider.main()
+    except Exception as e:
+        Logger.error('刷新API数据失败[' + api_id + '] : ' + str(e))
+
+def showVersionInfo():
+    print(meta.project + ' ' + meta.version);
 
 def main():
+    #初始化
     sys.path.append("spiders");
-    Logger = Log(connection);
+    Logger = Log();
     if (len(sys.argv) == 1):
         #如果是命令行调用 走下面的流程
         print('''
@@ -20,15 +49,15 @@ def main():
 
             --version : show version information
         ''');
-        exit();
     elif (len(sys.argv) >= 2):
+        #命令行调用刷新API数据
         command = sys.argv[1];
         if (command == 'renew'):
             if (len(sys.argv) >= 3):
                 api_id = sys.argv[2];
-                Logger.info(u'尝试刷新' + api_id);
-                #未完成部分
+                renewData(api_id,Logger);
             else:
+                #参数缺失
                 print('''
                     Parameter Missed
 
@@ -36,20 +65,27 @@ def main():
 
                     Main.py renew <api_id>
                 ''')
-                exit();
         elif (command == '-version' or command == '--version' or command == 'version'):
-            print(meta.project + ' ' + meta.version);
-            exit();
-    
-    try:
-        with connection.cursor() as cursor:
-            sql = 'SELECT * FROM data'
-            cursor.execute(sql);
-            result = cursor.fetchall()
-            print(result);
+            showVersionInfo();
 
-    except:
-        print('ERROR')
+        elif (command == 'start'):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+            sock.bind(('localhost', 3939))  #socket监听3939端口
+            sock.listen(5) #设置最大允许连接数，各连接和server的通信遵循FIFO原则  
+            Logger.info('服务器已启动 监听端口 : ' + str(3939) + ' 正在等待连接'),
+            while True:
+                connection,address = sock.accept()
+                try:
+                    #创建线程处理新连接
+                    thread = threading.Thread(target=tcpLinkHandler, args=(connection, address, Logger))
+                    thread.start();
+                    #connection.settimeout(120);
+                except socket.timeout:
+                    Logger.error('socket连接超时')
+
+    exit();
+
+
 if __name__ == "__main__":
     main();
 
---
+++ spiders/ExampleSpider.py
@@ -0,0 +1,27 @@
+# Common Header
+import sys, time
+sys.path.append('..')
+from spider import Spider
+from bs4 import BeautifulSoup
+from urllib import request
+
+
+class ExampleSpider(Spider):
+    """Spider描述"""
+    api_id = '000000001'  # 声明API ID
+
+    def __init__(self):
+        super(ExampleSpider, self).__init__()  # 仅修改类名，不要修改其他
+
+    def main(self):
+        """在这里执行你的主抓取逻辑"""
+        url = 'http://www.wz121.com/WeatherForecast/ThreeHourForecast.htm'
+        response = self.getPage(url)
+        html = BeautifulSoup(response, "html.parser", from_encoding="utf-8")
+        node = html.find(attrs={"id": 'ctl00_ContentBody_newsContent'})
+        data = dict(forecast=node.text)
+        result = self.createEvent(data)
+
+
+if __name__ == '__main__':
+    pass
