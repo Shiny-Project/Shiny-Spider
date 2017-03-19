440bcf6e30d3c44c2ef2c1339765beb222113c9e
Eridanus Sora <sora@sound.moe>
2017-03-19 22:46:11 +0800

    fix: 上报阻塞

--- core/log.py
+++ core/log.py
@@ -1,6 +1,6 @@
 # Shiny 控制台日志输出
 
-import time, requests
+import time, requests, json, threading
 from core import config
 
 class bcolors:
@@ -22,27 +22,27 @@ class Log():
 
     def debug(self, text):
         """创建调试信息"""
-        self.send_log(6, text)
+        threading.Thread(target=self.send_log, args=(6, text))
         print('[DEBUG] [' + str(time.strftime(self.TIMEFORMAT, time.localtime())) + '] ' + text)
 
     def info(self, text):
         """创建提示信息"""
-        self.send_log(5, text)
+        threading.Thread(target=self.send_log, args=(5, text))
         print(bcolors.OKGREEN + '[INFO] [' + str(time.strftime(self.TIMEFORMAT, time.localtime())) + '] ' + text + bcolors.ENDC)
 
     def warning(self, text):
         """创建警告信息"""
-        self.send_log(4, text)
+        threading.Thread(target=self.send_log, args=(4, text))
         print(bcolors.WARNING + '[WARN] [' + str(time.strftime(self.TIMEFORMAT, time.localtime())) + '] ' + text + bcolors.ENDC)
 
     def error(self, text):
         """创建错误信息"""
-        self.send_log(3, text)
+        threading.Thread(target=self.send_log, args=(3, text))
         print(bcolors.FAIL + '[ERROR] [' + str(time.strftime(self.TIMEFORMAT, time.localtime())) + '] ' + text + bcolors.ENDC)
     
     def critical(self, text):
         """创建致命错误信息"""
-        self.send_log(2, text)
+        threading.Thread(target=self.send_log, args=(2, text))
         print(bcolors.FAIL + '[ERROR] [' + str(time.strftime(self.TIMEFORMAT, time.localtime())) + '] ' + text + bcolors.ENDC)
 
     def send_log(self, level, text):
@@ -56,7 +56,7 @@ class Log():
         }
 
         try:
-            response = requests.post(self.API, data=data, headers=headers)
+            response = requests.post(self.API, data=json.dumps(data), headers=headers)
         except Exception as e:
             print(bcolors.FAIL + '在上报错误信息时出现错误: 无法连接到日志服务器' + text + bcolors.ENDC)
 
