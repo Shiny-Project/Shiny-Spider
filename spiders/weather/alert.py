6e07cdee233dacf7eacbeec5ad98ea5ea00a4671
Eridanus Sora <haolinaihui@Gmail.com>
2017-06-29 21:54:16 +0800

    调整推送范围

--- spiders/weather/alert.py
+++ spiders/weather/alert.py
@@ -17,7 +17,7 @@ class AlertSpider(spider.Spider):
         tree = BeautifulSoup(text, 'xml')
         alert_list = tree.find_all('div', class_=class_is_even_or_odd)
         for alert in alert_list:
-            if "发布" in alert.a.text and "红色" in alert.a.text and "县" not in alert.a.text and "自治州" not in alert.a.text:
+            if "发布" in alert.a.text and "红色" in alert.a.text and "县" not in alert.a.text and "自治州" not in alert.a.text and "区" not in alert.a.text:
                 self.record(3, {
                     "title": "中国·红色气象预警速报",
                     "link": "http://www.nmc.cn" + alert.a.get('href'),
