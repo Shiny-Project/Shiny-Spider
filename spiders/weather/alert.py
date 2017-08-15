063fa846d1a947edff0da85e72cb51936fd4c2cf
Eridanus Sora <haolinaihui@Gmail.com>
2017-08-15 10:51:54 +0800

    +CEIC spider

--- spiders/weather/alert.py
+++ spiders/weather/alert.py
@@ -1,29 +1,32 @@
 from core import spider
 from bs4 import BeautifulSoup
 
-def class_is_even_or_odd(css_class = []):
+
+def class_is_even_or_odd(css_class=[]):
     if css_class is None:
         return False
     return 'even' in css_class or 'odd' in css_class
 
+
 class AlertSpider(spider.Spider):
     """CMA 全国气象预警"""
+
     def __init__(self):
         super(AlertSpider, self).__init__()
         self.name = 'AlertSpider'
-    
+
     def main(self):
         text = self.fetch('http://www.nmc.cn/f/alarm.html').decode('utf-8')
         tree = BeautifulSoup(text, 'xml')
         alert_list = tree.find_all('div', class_=class_is_even_or_odd)
         for alert in alert_list:
-            if "发布" in alert.a.text and "红色" in alert.a.text and "县" not in alert.a.text and "自治州" not in alert.a.text and "区" not in alert.a.text:
+            if "上海" in alert.a.text or "杭州" in alert.a.text or "郑州"  in alert.a.text and "区" not in alert.a.text and "县" not in alert.a.text:
                 self.record(3, {
-                    "title": "中国·红色气象预警速报",
+                    "title": "中国·气象预警速报",
                     "link": "http://www.nmc.cn" + alert.a.get('href'),
                     "content": alert.find(class_="date").text + " " + alert.a.text,
                     "cover": alert.img.get('src')
                 })
 
-    def check(self, timestamp): 
-        return self.check_expiration(timestamp, 180) 
+    def check(self, timestamp):
+        return self.check_expiration(timestamp, 180)
---
+++ spiders/weather/ceic.py
@@ -0,0 +1,26 @@
+from core import spider
+import json
+
+
+class CEICSpider(spider.Spider):
+    """中国地震台网"""
+
+    def __init__(self):
+        super(CEICSpider, self).__init__()
+        self.name = 'CEICSpider'
+
+    def main(self):
+        result = self.fetch(
+            "http://www.ceic.ac.cn/ajax/search?page=1&&start=&&end=&&jingdu1=73&&jingdu2=135&&weidu1=15&&weidu2=53&&height1=&&height2=&&zhenji1=&&zhenji2=").decode("utf-8")
+        data = json.loads(result[1:-1])
+        for item in data['shuju']:
+            if float(item["M"]) > 5.0:
+                self.record(3, {
+                    "title": "中国地震速报",
+                    "link": "http://news.ceic.ac.cn/" + item["NEW_DID"],
+                    "content": "{time} 于 {E}E, {N}N aka. {place} 发生 {M} 地震".format(time=item["O_TIME"], E=item["EPI_LON"], N=item["EPI_LAT"], place=item["LOCATION_C"], M=item["M"]),
+                    "cover": "http://maps.google.cn/maps/api/staticmap?center={N},{E}&zoom=6&size=128x128&maptype=roadmap&markers=color:red|{N},{E}&sensor=false".format(N=item["EPI_LAT"], E=item["EPI_LON"])
+                })
+
+    def check(self, timestamp):
+        return self.check_expiration(timestamp, 180)
