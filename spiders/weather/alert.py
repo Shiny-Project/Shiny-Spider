55ad791805715e3e8e487e56402120adf7af0a11
Eridanus Sora <haolinaihui@Gmail.com>
2017-08-15 20:09:30 +0800

    fix condition

--- spiders/weather/alert.py
+++ spiders/weather/alert.py
@@ -20,7 +20,7 @@ class AlertSpider(spider.Spider):
         tree = BeautifulSoup(text, 'xml')
         alert_list = tree.find_all('div', class_=class_is_even_or_odd)
         for alert in alert_list:
-            if "上海" in alert.a.text or "杭州" in alert.a.text or "郑州"  in alert.a.text and "区" not in alert.a.text and "县" not in alert.a.text:
+            if ("上海" in alert.a.text or "杭州" in alert.a.text or "郑州" in alert.a.text) and ("市气象台" in alert.a.text) and ("区" not in alert.a.text and "县" not in alert.a.text):
                 self.record(3, {
                     "title": "中国·气象预警速报",
                     "link": "http://www.nmc.cn" + alert.a.get('href'),
