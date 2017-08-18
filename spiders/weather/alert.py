fc50dd0028363098903f7179dda970754438c4ec
root <root@ubuntu.members.linode.com>
2017-08-18 06:47:42 +0000

    add timestamp to request

--- spiders/weather/alert.py
+++ spiders/weather/alert.py
@@ -1,5 +1,5 @@
 from core import spider
-import json
+import json, time
 
 
 def class_is_even_or_odd(css_class=[]):
@@ -16,7 +16,7 @@ class AlertSpider(spider.Spider):
         self.name = 'AlertSpider'
 
     def main(self):
-        result = self.fetch("http://www.12379.cn/data/alarm_list_all.html").decode("utf-8")
+        result = self.fetch("http://www.12379.cn/data/alarm_list_all.html?_=" + str(int(time.time())) ).decode("utf-8")
         data = json.loads(result)
         for item in data["alertData"]:
             if ("杭州" in item["description"] or "上海" in item["description"] or "郑州" in item["description"]) and ("县" not in item["description"] and "区" not in item["description"]):
