bba7709da7a612a2b79438498f7b61c676a71d35
Eridanus Sora <haolinaihui@Gmail.com>
2018-03-04 20:43:54 +0800

    lv4 when red alert

--- spiders/weather/alert.py
+++ spiders/weather/alert.py
@@ -14,11 +14,18 @@ class AlertSpider(spider.Spider):
         data = json.loads(result)
         for item in data["alertData"]:
             if ("杭州" in item["headline"] or "上海" in item["headline"] or "郑州" in item["headline"]) and ("县" not in item["headline"] and "区" not in item["headline"]):
-                self.record(3, {
-                    "title": "中国·预警速报",
-                    "content": item["headline"],
-                    "link": "http://www.12379.cn/data/alarmcontent.shtml?file={}.html".format(item["identifier"])
-                })
+                if ("红色" in item["headline"]):
+                    self.record(4, {
+                        "title": "中国·预警速报",
+                        "content": item["headline"],
+                        "link": "http://www.12379.cn/data/alarmcontent.shtml?file={}.html".format(item["identifier"])
+                    })
+                else:
+                    self.record(3, {
+                        "title": "中国·预警速报",
+                        "content": item["headline"],
+                        "link": "http://www.12379.cn/data/alarmcontent.shtml?file={}.html".format(item["identifier"])
+                    })
 
     def check(self, timestamp):
         return self.check_expiration(timestamp, 180)
