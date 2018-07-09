77a8f06dbbb3bf3c18a8bf03d8b2c9666b8921ca
Eridanus Sora <haolinaihui@Gmail.com>
2018-07-09 09:29:17 +0800

    fix date bug

--- spiders/weather/cma_alert.py
+++ spiders/weather/cma_alert.py
@@ -1,6 +1,6 @@
 from core import spider
 from bs4 import BeautifulSoup
-import datetime, pytz
+import datetime, pytz, time
 
 
 def class_is_warning(css_class=[]):
@@ -20,26 +20,27 @@ class CMAAlertSpider(spider.Spider):
         result = self.fetch("http://www.nmc.cn/").decode('utf-8')
         soup = BeautifulSoup(result)
         warning_list = soup.find_all('li', class_='waring')
+        tz = pytz.timezone('Asia/Shanghai')
         if len(warning_list) > 0:
             for item in soup.find_all('li', class_='waring')[0].find_all('a'):
                 if '蓝色' in item.attrs['title'] or '黄色' in item.attrs['title']:
                     self.record(3, {
                         "title": "CMA·全国级预警速报",
                         "link": 'http://www.nmc.cn' + item.attrs['href'],
-                        "content": item.attrs['title'] + '(' + pytz.timezone('Asia/Shanghai').localize(datetime.datetime.now()).strftime('%Y-%m-%d') + ')'
+                        "content": item.attrs['title'] + '(' + datetime.datetime.fromtimestamp(int(time.time()), tz).strftime('%Y-%m-%d') + ')'
                     })
                 if '橙色' in item.attrs['title'] or '红色' in item.attrs['title']:
                     if '解除' in item.attrs['title']:
                         self.record(3, {
                             "title": "CMA·全国级预警速报",
                             "link": "http://www.nmc.cn" + item.attrs['href'],
-                            "content": item.attrs['title'] + '(' + pytz.timezone('Asia/Shanghai').localize(datetime.datetime.now()).strftime('%Y-%m-%d') + ')'
+                            "content": item.attrs['title'] + '(' + datetime.datetime.fromtimestamp(int(time.time()), tz).strftime('%Y-%m-%d') + ')'
                         })
                     else:
                         self.record(4, {
                             "title": "CMA·全国级预警速报",
                             "link": "http://www.nmc.cn" + item.attrs['href'],
-                            "content": item.attrs['title'] + '(' + pytz.timezone('Asia/Shanghai').localize(datetime.datetime.now()).strftime('%Y-%m-%d') + ')'
+                            "content": item.attrs['title'] + '(' + datetime.datetime.fromtimestamp(int(time.time()), tz).strftime('%Y-%m-%d') + ')'
                         })
 
     def check(self, timestamp):
